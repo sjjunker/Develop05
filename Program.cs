cafae99850acc3efd20de1cf4759d8a6a697b545
Sandi Junker <sandi.junker@outlook.com>
2024-06-11 16:27:21 -0500

    Initial commit push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+//Extra creativity: Added 5 levels based on score. Required a Levels class, determine level method, list, and console.writeline witihin the GoalManager class.
+class Program
+{
+    static void Main(string[] args)
+    {
+        GoalManager newGoalManager = new GoalManager();
+
+        newGoalManager.Start();
+    }
+}
