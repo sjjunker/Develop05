c530afaebeec28e8d34f4b917823187d289e4464
Sandi Junker <sandi.junker@outlook.com>
2024-06-11 16:29:25 -0500

    Initial commit

--- obj/Debug/net7.0/Develop05.AssemblyInfo.cs
+++ obj/Debug/net7.0/Develop05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Develop05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cafae99850acc3efd20de1cf4759d8a6a697b545")]
 [assembly: System.Reflection.AssemblyProductAttribute("Develop05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Develop05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
